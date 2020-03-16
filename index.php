3c3941065c9951ef2887261aa750a31156c295d5
Dilan Perera <r.dilanperera@gmail.com>
2020-03-16 21:45:54 +0530

    fine

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="icon" href="fav.png" type="image/png" sizes="32x32">
+    <title>Theia-Project</title>
   </head>
   <body style="background:#e9ecef">
     <div class="jumbotron">
